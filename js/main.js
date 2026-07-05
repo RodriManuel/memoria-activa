3fd5475a40a76627a8956dc89c1dc5fbc8b90494
Ro <rodrigomanuel.perez@estudiantes.unahur.edu.ar>
2026-07-05 20:06:17 -0300

    feat: agrega la lógica del componente interactivo del menú responsive en 'main.js'

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log("Hola Mundo!")
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    // Esto le pone o le saca la clase 'active' al nav cada vez que haces clic
+    navLinks.classList.toggle('active');
+});
