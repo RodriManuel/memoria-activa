a3b0cf3c1a0a0cc397d2b76a74f21197184181d4
Ro <rodrigomanuel.perez@estudiantes.unahur.edu.ar>
2026-07-28 15:52:47 -0300

    feat: se elimina el botón si la cita no supera cierto límite de carácteres

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ menuToggle.addEventListener('click', () => {
     // Esto le pone o le saca la clase 'active' al nav cada vez que haces clic
     navLinks.classList.toggle('active');
 });
+
+
+// Leer Más/Menos en los testimonios.
+let limiteDeCaracteres = 192;
+let citas = document.querySelectorAll(".cita");
+
+citas.forEach(cita => {
+    
+    let blockquote = cita.closest("blockquote");
+    
+    let boton = blockquote?.nextElementSibling;
+
+    if (boton && cita.textContent.length < limiteDeCaracteres) {
+        boton.style.display = "none";
+    }
+})
